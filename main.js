8ec38689f5d9f70cfa33e6e5a640a0fd846f3197
Haciellp <108480136+Haciellp@users.noreply.github.com>
2022-12-21 10:53:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,56 +1,35 @@
 window.onload=inicio;
 let sumaCorrecta,a,b;
 
-function inicio(){
+  function inicio(){
+    preguntar();
+    document.querySelector(".btn").onclick=comprobar;
+    document.querySelector("#tres").onkeydown=teclado;
+  }
 
-
-  preguntar();
-  document.querySelector(".btn").onclick=comprobar;
-  document.querySelector("#tres").onkeydown=teclado;
-}
-
-    function teclado(e){
-      let tecla= e.key;
-      if(tecla=="Enter"){
-        comprobar()
-      }
+  function teclado(e){
+    let tecla= e.key;
+    if(tecla=="Enter"){
+      comprobar()
     }
+  }
 
-    function comprobar(){
-      let c=Number(document.querySelector("#tres").value);
-        if(c==sumaCorrecta){
-          document.querySelector(".answer").insertAdjacentHTML("beforeend",`<div class="verde">Correcto</div>`);
-        }else{
-          document.querySelector(".answer").insertAdjacentHTML("beforeend",`<div class="rojo">El resultado de ${a} + ${b}= ${sumaCorrecta}</div>`);
-        }    
-        preguntar();
+  function comprobar(){
+    let c=Number(document.querySelector("#tres").value);
+      if(c==sumaCorrecta){
+        document.querySelector(".answer").insertAdjacentHTML("beforeend",`<div class="verde">Correcto</div>`);
+      }else{
+        document.querySelector(".answer").insertAdjacentHTML("beforeend",`<div class="rojo">El resultado de ${a} + ${b}= ${sumaCorrecta}</div>`);
+      }    
+      preguntar();
+  }
+
+  function preguntar(){
+    a=Math.floor(Math.random()*10);
+    b=Math.floor(Math.random()*10);
+    sumaCorrecta=a+b;
+    document.querySelector("#uno").value=a;
+    document.querySelector("#dos").value=b;
+    document.querySelector("#tres").value="";
+    document.querySelector("#tres").focus();
     }
-
-    function preguntar(){
-      a=Math.floor(Math.random()*10);
-      b=Math.floor(Math.random()*10);
-      sumaCorrecta=a+b;
-      document.querySelector("#uno").value=a;
-      document.querySelector("#dos").value=b;
-      document.querySelector("#tres").value="";
-      document.querySelector("#tres").focus();
-       }
-
-       
-       
-       
-       
-       
-       
-
-      function myGreeting() {
-        document.getElementById("demo").innerHTML = "Happy Birthday!"
-      }
-       
-       
-       
-       
-       
-       
-      
-    
